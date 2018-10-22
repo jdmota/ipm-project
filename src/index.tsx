65a02f613e8303253017311a9e9741748a8f9f5b
João Mota <jdmota@outlook.pt>
2018-10-22 17:20:06 +0100

    Use new typography variants

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 
+// https://material-ui.com/style/typography/#migration-to-typography-v2
+const win: any = window;
+win.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+
 ReactDOM.render(
   <App />,
   document.getElementById( "root" )
