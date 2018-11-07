ef4e9a5ebcb9e14c2643b6e46e96032610acaa19
João Mota <jdmota@outlook.pt>
2018-11-07 17:48:44 +0000

    WIP

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,27 +9,6 @@ type AppState = {
   rightAvancedSearchDrawer: boolean
 };
 
-const styles = theme => ( {
-  root: {
-    display: "flex"
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1
-  },
-  drawer: {
-    width: 240,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: 240
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3
-  },
-  toolbar: theme.mixins.toolbar
-} );
-
 class App extends React.Component<{}, AppState> {
 
   state = {
