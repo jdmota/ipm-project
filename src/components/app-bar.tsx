e9ffbe7f2dbb814b0e98c35ef95525cbeb64f300
Unknown <pmapsp@gmail.com>
2018-11-08 19:07:48 +0000

    Advances search WIP

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -8,7 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchBar from "./search-bar";
 
-const styles = {
+const styles = theme => ( {
   root: {
     flexGrow: 1,
   },
@@ -23,15 +23,18 @@ const styles = {
     width: "100%",
     marginLeft: 24,
     marginRight: 24
+  },
+  appBar: {
+    zIndex: 2
   }
-};
+} );
 
 type OurAppBarProps = {
   classes: {
     root: string,
     grow: string,
     menuButton: string,
-    search: string,
+    appBar: string
     loginButton: string
   },
   onLeftDrawerToggle: () => void
@@ -43,7 +46,7 @@ function OurAppBar( props: OurAppBarProps ) {
   const { classes, onLeftDrawerToggle } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar className={classes.appBar}>
         <Toolbar>
           <IconButton className={classes.menuButton} onClick={onLeftDrawerToggle} color="inherit" aria-label="Menu">
             <MenuIcon />
