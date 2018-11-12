f90a8f372ab7fba3c8e22715ddef8a3d214d5de3
João Mota <jdmota@outlook.pt>
2018-11-12 11:52:11 +0000

    Fix hot stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { hot } from "react-hot-loader";
-import App from "./components/app";
-import { MuiPickersUtilsProvider } from "material-ui-pickers";
-import DateFnsUtils from "@date-io/date-fns";
-
-// https://material-ui.com/style/typography/#migration-to-typography-v2
-const win: any = window;
-win.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
-
-const App2 = hot( module )( () => (
-  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-    <App />
-  </MuiPickersUtilsProvider>
-) );
+import Root from "./root";
 
 ReactDOM.render(
-  <App2 />,
+  <Root />,
   document.getElementById( "root" )
 );
