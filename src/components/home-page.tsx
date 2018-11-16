6bd4828f5c183f28c34558f635ba5af6e0cd797e
Daniela Prado <d.prado@campus.fct.unl.pt>
2018-11-16 02:14:47 +0000

    Home Page WIP (3)

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import Button from "@material-ui/core/Button";
+import Slider from "./slider";
 
 const styles = theme => ( {
 
@@ -39,12 +40,22 @@ const styles = theme => ( {
     height: 500,
     margin: "auto",
   },
-  imgContiner: {
-    flex: "0 1 25%",
-    width: "100%",
-    "@media all and (min-width: 1300px)": {
-      marginLeft: 30
+  flexContainerEvent: {
+    display: "flex",
+    flexFlow: "row wrap",
+    justifyContent: "space-around",
+    "@media all and (max-width: 1300px)": {
+      flexFlow: "column"
+    },
+    "& > div": {
+      marginBottom: 20
     }
+  },
+  imageContainer: {
+    width: "94%",
+    height: 450,
+    marginTop: 15,
+    marginBottom: 15
   },
   card: {
     maxWidth: 500,
@@ -66,8 +77,32 @@ const styles = theme => ( {
     marginTop: 15,
     marginBottom: 0
   },
+  cardHighlight: {
+    maxWidth: 500,
+    // height: 227
+  },
+  imageHighlight: {
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    height: 110
+  },
+  forceHighlight: {
+    paddingTop: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingBottom: 0
+  },
+  sizeHighlight: {
+    // width: 300,
+    height: 500,
+    marginTop: 15,
+    marginBottom: 0
+  },
   next: {
     marginTop: 0
+  },
+  outside: {
+    marginLeft: 100,
+    marginRight: 40
   }
 } );
 
@@ -76,18 +111,18 @@ function HomePage( props: {classes: any, events: Event[]} ) {
   const { classes } = props;
   const { title, description, date, location, type, priceUnit } = props.events[ 0 ];
 
-  return <div>
+  return <div className={classes.outside}>
     <div>
       <Typography variant="h4" color="inherit" >
         <b>Highlights</b>
       </Typography>
 
-      <div className={classes.size}>
-        <Card className={classes.card}>
+      {/* <div className={classes.sizeHighlight}>
+        <Card className={classes.cardHighlight}>
           <CardHeader
-            className={classes.force}
+            className={classes.forceHighlight}
             avatar={
-              <div className={classes.image}>
+              <div className={classes.imageHighlight}>
                 <img height={110} src={"/images/NOS/nos_alive_4.jpg"}/>
               </div>
             }
@@ -95,6 +130,9 @@ function HomePage( props: {classes: any, events: Event[]} ) {
             subheader={`${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`}
           />
         </Card>
+          </div>*/}
+      <div className={classes.imageContainer}>
+        <Slider/>
       </div>
 
     </div>
@@ -107,7 +145,7 @@ function HomePage( props: {classes: any, events: Event[]} ) {
 
         <div className={classes.size}>
           <Card className={classes.card}>
-          
+
             <CardHeader
               className={classes.force}
               avatar={
@@ -133,7 +171,10 @@ function HomePage( props: {classes: any, events: Event[]} ) {
               }
               title={title}
               subheader={`${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`}
-            />
+            ></CardHeader>
+            <Button variant="contained" className={classes.button}>
+              Default
+            </Button>
           </Card>
         </div>
       </div>
