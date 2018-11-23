d3af73b915b2d8a95c3775e9d79b33f09a397458
João Mota <jdmota@outlook.pt>
2018-11-23 21:59:36 +0000

    [Deploy]

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,40 @@ const path = require( "path" );
 const webpack = require( "webpack" );
 const CopyWebpackPlugin = require( "copy-webpack-plugin" );
 
-const PRODUCTION = false;
+function Replace( { test, callback } ) {
+  return {
+    apply( compiler ) {
+      const dirname = path.resolve( __dirname );
+      compiler.plugin( "emit", ( compilation, done ) => {
+        for ( const basename in compilation.assets ) {
+          const filepath = path.resolve( dirname, basename );
+          if ( test.test( filepath ) ) {
+            const asset = compilation.assets[ basename ];
+            const origFn = asset.source();
+            asset.source = () => callback( origFn );
+            asset.size = () => asset.source().length;
+          }
+        }
+        done();
+      } );
+    }
+  };
+}
+
+const DEPLOY = false;
 
 module.exports = {
-  mode: PRODUCTION ? "production" : "development",
+  mode: DEPLOY ? "production" : "development",
   entry: "./src/index.tsx",
   output: {
-    path: PRODUCTION ? path.join( __dirname, "docs" ) : path.join( __dirname, "dist" ),
+    path: DEPLOY ? path.join( __dirname, "docs" ) : path.join( __dirname, "dist" ),
     filename: "index.js",
-    publicPath: "/"
+    publicPath: DEPLOY ? "/ipm-project" : "/"
   },
   resolve: {
     extensions: [ ".ts", ".tsx", ".js", ".jsx" ]
   },
-  devtool: "source-map",
+  devtool: DEPLOY ? false : "source-map",
   module: {
     rules: [
       {
@@ -33,8 +53,14 @@ module.exports = {
       { from: "src/404.html", to: "404.html" },
       { from: "src/images", to: "images" }
     ] ),
+    DEPLOY && new Replace( {
+      test: /\.html$/,
+      callback( source ) {
+        return Buffer.from( source.toString().replace( /\/index\.js/g, "/ipm-project/index.js" ) );
+      }
+    } ),
     new webpack.HotModuleReplacementPlugin()
-  ],
+  ].filter( Boolean ),
   devServer: {
     contentBase: path.join( __dirname, "dist" ),
     port: 9000,
