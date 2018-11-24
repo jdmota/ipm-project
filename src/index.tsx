cb71083256d4e5f8145e4bf5a4a36fc7d3e72a5f
João Mota <jdmota@outlook.pt>
2018-11-24 16:50:08 +0000

    Make sure react-hot is not on production

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import Root from "./root";
+import { MuiPickersUtilsProvider } from "material-ui-pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import App from "./components/app";
 import store from "./data/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Root />
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <App />
+    </MuiPickersUtilsProvider>
   </Provider>,
   document.getElementById( "root" )
 );
